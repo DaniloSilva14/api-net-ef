9777cef0fc360672bb34075d679d6475a273c8a8
DOMVS-FZSM862\AS-Informatica <danilo.silva@domvsit.com.br>
2022-08-16 11:31:43 -0300

    Cap 2

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O campo Genero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no min 1 minuto e no max 600")]
+        public int Duracao { get; set; }
+    }
+}
